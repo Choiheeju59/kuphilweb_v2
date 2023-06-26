18b51ab9c12f3320d1bfab894be9961cec0e4d44
Choiheeju59 <edouardlalo@naver.com>
2023-06-26 23:45:10 +0900

    [Add] Remote kuweb.v2 project

---
+++ src/Components/main/Tips.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+
+const Tips = () => {
+  return <div></div>;
+};
+
+export default Tips;
---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Tips from "../Components/main/Tips";
+
+const Main = () => {
+  return (
+    <>
+      <Tips />
+    </>
+  );
+};
+
+export default Main;
