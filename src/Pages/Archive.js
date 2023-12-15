a49597f0f5985486de6b8d23ea64b262f6a21ff4
serin301 <63085375+serin301@users.noreply.github.com>
2023-12-15 22:25:25 +0900

    FEAT: Archive 페이지 상세 구현

--- src/Pages/Archive.js
+++ src/Pages/Archive.js
@@ -1,6 +1,6 @@
 import Footer from '../Components/footer/Footer';
 import Header from '../Components/header/Header';
-import PosterContent from '../Components/archive/PosterContent';
+import PosterContent from "../Components/archive/PosterContent";
 import React from "react";
 import TitleGradient from '../Components/TitleGradient';
 import styled from 'styled-components';
@@ -17,16 +17,7 @@ const Archive = () => {
                         color="linear-gradient(91.48deg, #ECFDE5 0%, rgba(189, 222, 236, 0.31) 100%)"
                     />
                     <MainContent>
-                        <PrevBtn/>
-                        <PosterContainer>
-                            <PosterContent/>
-                            <PosterContent/>
-                            <PosterContent/>
-                            <PosterContent/> 
-                            <PosterContent/>
-                            <PosterContent/>
-                        </PosterContainer>
-                        <NextBtn/>
+                        <PosterContent/>
                     </MainContent>
                 </Contents>
             </Wrap>
@@ -61,59 +52,12 @@ const MainContent = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    flex-direction: column;
     
     @media screen and (max-width: 767px){
       padding: 0 5%;
       margin-top: 30px;
     } 
 `;
-
-const PrevBtn = styled.div`
-    cursor: pointer;
-    content: '';
-    display: inline-block;
-    width: 5px;
-    height: 5px;
-    border-top: 3px solid #222;
-    border-right: 3px solid #222;
-    transform: rotate(225deg);
-    padding: 20px;
-
-    @media screen and (max-width: 767px){
-        width: 2px;
-        height: 2px;
-        padding: 10px;
-    }
-`
-const NextBtn = styled.div`
-    cursor: pointer;
-    content: '';
-    display: inline-block;
-    width: 5px;
-    height: 5px;
-    border-top: 3px solid #222;
-    border-right: 3px solid #222;
-    transform: rotate(45deg);
-    padding: 20px;
-
-    @media screen and (max-width: 767px){
-        width: 2px;
-        height: 2px;
-        padding: 10px;
-    }
-`
-
-const PosterContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 200px);
-  grid-template-rows: repeat(2, 400px);
-  gap: 30px 40px;
-  margin: 0 20px;
-
-  @media screen and (max-width: 767px){
-    grid-template-columns: repeat(1, 120px);
-    grid-template-rows: repeat(6, 240px);
-  }
-`
 
 export default Archive;
