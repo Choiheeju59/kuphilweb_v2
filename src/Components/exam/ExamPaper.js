f5e6580a397815cc3a5080a31f1d6847ce4bd6f0
rkddusen <k99endus@naver.com>
2024-01-07 22:20:27 +0900

    클래식 능력고사 데이터 연동 및 결과 화면 디자인

--- src/Components/exam/ExamPaper.js
+++ src/Components/exam/ExamPaper.js
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from "react";
 import styled from 'styled-components';
+import { getExamData } from '../../utils/api';
+import { useParams } from 'react-router-dom';
 
 const ExamPaper = (props) => {
-  const { id, question, problem, handleClickEvent, score } = props;
+  const { id, question, choices, handleClickEvent, score } = props;
+  const params = useParams();
   const [loading, setLoading] = useState(false);
+  const [openAnswer, setOpenAnswer] = useState(false);
+  const [answerDiv, setAnswerDiv] = useState(null);
 
   useEffect(() => {
     if(id > 10){
@@ -17,11 +22,43 @@ const ExamPaper = (props) => {
       // 3초 뒤 로딩 해제
       const timeoutId = setTimeout(() => {
         setLoading(false);
-      }, 2000);
+      }, 1000);
 
       return () => clearTimeout(timeoutId);
     }
   }, [loading]);
+
+  useEffect(() => {
+    if(openAnswer){
+      getExamData(params.id)
+        .then((res) => {
+          console.log(res.data);
+          let _answer = [];
+          res.data.map((v, i) => {
+            _answer.push(
+              <AnswerBox key={i}>
+                <div>
+                  <AnswerQuestion>{v.id}. {v.question}</AnswerQuestion>
+                </div>
+                <div>
+                  <div>
+                    {v.choices.map((cv, ci) =>
+                      <AnswerProblemItem key={ci} check={v.answer === (ci + 1)}>
+                        {ci + 1}. {cv}
+                      </AnswerProblemItem>
+                    )}
+                  </div>
+                </div>
+              </AnswerBox>
+            );
+            setAnswerDiv(_answer);
+          })
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+    }
+  }, [openAnswer])
 
   return (
     <StyledExamPaper>
@@ -31,7 +68,7 @@ const ExamPaper = (props) => {
             {id ? 
               <>
                 <Progress>{id} / 10</Progress>
-                <Question>{id}. {question} [10점]</Question>
+                <Question>{id}. {question}</Question>
               </>
             :
               <Question>{question}</Question>
@@ -39,7 +76,7 @@ const ExamPaper = (props) => {
           </div>
           <div>
             <ProblemList>
-              {problem.map((v, i) =>
+              {choices.map((v, i) =>
                 <ProblemItem key={i} onClick={() => {handleClickEvent(i + 1)}}>
                   {v}
                 </ProblemItem>
@@ -65,6 +102,14 @@ const ExamPaper = (props) => {
                   <ProblemItem onClick={() => {handleClickEvent(2)}}>종료하기</ProblemItem>
                 </ProblemList>
               </div>
+              <ShowAnswer>
+                <ShowAnswerBtn onClick={() => setOpenAnswer(v => !v)}>{!openAnswer ? '정답 보기 ▼' : '정답 접기 ▲'}</ShowAnswerBtn>
+                <AnswerArea openanswer={openAnswer}>
+                  {/* 시험 정답 */}
+                  {answerDiv}
+                  <ShowAnswerBtn onClick={() => setOpenAnswer(false)}>정답 접기 ▲</ShowAnswerBtn>
+                </AnswerArea>
+              </ShowAnswer>
             </>
           )}
         </>
@@ -78,8 +123,9 @@ const StyledExamPaper = styled.div`
   margin: 0 auto;
   padding: 50px 30px 30px 30px;
   box-sizing: border-box;
-  border-radius: 5px;
-  background-image: url('/images/exam/exam_paper.jpg');
+  border-radius: 15px;
+  //background-image: url('/images/exam/exam_paper.jpg');
+  background-color: #dddddd;
   text-align: start;
   box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.15);
   @media screen and (max-width: 767px){
@@ -95,8 +141,10 @@ const Progress = styled.p`
     font-size: 12px;
   }
 `;
-const Question = styled.p`
+const Question = styled.div`
   font-size: 22px;
+  white-space: pre-line;
+  line-height: 1.2;
 
   @media screen and (max-width: 767px){
     font-size: 18px;
@@ -146,6 +194,35 @@ const Loading = styled.div`
   width: 100%;
   text-align: center;
   padding: 50px 0;
+`;
+
+const ShowAnswer = styled.div`
+  text-align: center;
+  margin-top: 50px;
+`;
+const ShowAnswerBtn = styled.p`
+  display: inline;
+  text-align: center;
+  &:hover{
+    color: #555555;
+    cursor: pointer;
+  }
+`;
+const AnswerArea = styled.div`
+  display: ${props => props.openanswer ? 'block' : 'none'};
+`;
+const AnswerBox = styled.div`
+  text-align: start;
+  margin: 30px 0;
+`;
+const AnswerQuestion = styled.div`
+  font-size: 20px;
+  white-space: pre-line;
+  line-height: 1.2;
+`;
+const AnswerProblemItem = styled.div`
+  margin: 10px 0;
+  color: ${props => props.check ? 'red' : 'black'};
 `;
 
 export default ExamPaper;
