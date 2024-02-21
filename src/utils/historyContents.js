75421be847eb7d6e67ff85590240c31d4fc4a845
serin301 <63085375+serin301@users.noreply.github.com>
2024-02-22 01:53:30 +0900

    FEAT: 로컬 데이터 순서 변경

--- src/utils/historyContents.js
+++ src/utils/historyContents.js
@@ -1,28 +1,23 @@
 export const HistoryContents = [
     {   
         year: 2023,
-        date: '02월 26일',
-        content: '제 40회 정기연주회'
-    },
-    {   
-        year: 2023,
         date: '08월 25일',
         content: '제 41회 정기연주회'
     },
     {   
-        year: 2022,
-        date: '02월 27일',
-        content: '제 38회 정기연주회'
+        year: 2023,
+        date: '02월 26일',
+        content: '제 40회 정기연주회'
     },
-    {
+    {   
         year: 2022,
         date: '08월 27일',
         content: '제 39회 정기연주회'
     },
     {
-        year: 2021,
-        date: '08월 28일',
-        content: '제 37회 정기연주회'
+        year: 2022,
+        date: '02월 27일',
+        content: '제 38회 정기연주회'
     },
     {
         year: 2021,
@@ -30,9 +25,9 @@ export const HistoryContents = [
         content: '광진예술제 (나루와 함께 하는 Kuphil 영화 여행)'
     },
     {
-        year: 2020,
-        date: '02월 28일',
-        content: '(취소) 제 35회 정기연주회'
+        year: 2021,
+        date: '08월 28일',
+        content: '제 37회 정기연주회'
     },
     {
         year: 2020,
@@ -40,14 +35,14 @@ export const HistoryContents = [
         content: '(취소) 제 36회 정기연주회'
     },
     {
-        year: 2019,
-        date: '03월 01일',
-        content: '제 33회 정기연주회'
+        year: 2020,
+        date: '02월 28일',
+        content: '(취소) 제 35회 정기연주회'
     },
     {
         year: 2019,
-        date: '05월 16일',
-        content: '대동제 연주'
+        date: '12월',
+        content: '제 1회 동아리 경진대회 대상 수상'
     },
     {
         year: 2019,
@@ -56,13 +51,13 @@ export const HistoryContents = [
     },
     {
         year: 2019,
-        date: '12월',
-        content: '제 1회 동아리 경진대회 대상 수상'
+        date: '05월 16일',
+        content: '대동제 연주'
     },
     {
-        year: 2018,
-        date: '03월 08일',
-        content: '제 31회 정기연주회'
+        year: 2019,
+        date: '03월 01일',
+        content: '제 33회 정기연주회'
     },
     {
         year: 2018,
@@ -70,9 +65,9 @@ export const HistoryContents = [
         content: '제 32회 정기연주회'
     },
     {
-        year: 2017,
-        date: '03월 10일',
-        content: '제 29회 정기연주회'
+        year: 2018,
+        date: '03월 08일',
+        content: '제 31회 정기연주회'
     },
     {
         year: 2017,
@@ -80,9 +75,9 @@ export const HistoryContents = [
         content: '제 30회 정기연주회'
     },
     {
-        year: 2016,
-        date: '03월 03일',
-        content: '제 27회 정기연주회'
+        year: 2017,
+        date: '03월 10일',
+        content: '제 29회 정기연주회'
     },
     {
         year: 2016,
@@ -90,9 +85,9 @@ export const HistoryContents = [
         content: '제 28회 정기연주회'
     },
     {
-        year: 2015,
-        date: '02월 27일',
-        content: '제 25회 정기연주회'
+        year: 2016,
+        date: '03월 03일',
+        content: '제 27회 정기연주회'
     },
     {
         year: 2015,
@@ -100,9 +95,9 @@ export const HistoryContents = [
         content: '제 26회 정기연주회'
     },
     {
-        year: 2014,
-        date: '03월 06일',
-        content: '제 22회 정기연주회'
+        year: 2015,
+        date: '02월 27일',
+        content: '제 25회 정기연주회'
     },
     {
         year: 2014,
@@ -110,9 +105,9 @@ export const HistoryContents = [
         content: '제 23회 정기연주회'
     },
     {
-        year: 2013,
-        date: '03월 07일',
-        content: '제 20회 정기연주회'
+        year: 2014,
+        date: '03월 06일',
+        content: '제 22회 정기연주회'
     },
     {
         year: 2013,
@@ -120,9 +115,9 @@ export const HistoryContents = [
         content: '제 21회 정기연주회'
     },
     {
-        year: 2012,
-        date: '03월 18일',
-        content: '제 18회 정기연주회'
+        year: 2013,
+        date: '03월 07일',
+        content: '제 20회 정기연주회'
     },
     {
         year: 2012,
@@ -130,9 +125,9 @@ export const HistoryContents = [
         content: '제 19회 정기연주회'
     },
     {
-        year: 2011,
+        year: 2012,
         date: '03월 18일',
-        content: '제 16회 정기연주회'
+        content: '제 18회 정기연주회'
     },
     {
         year: 2011,
@@ -140,9 +135,9 @@ export const HistoryContents = [
         content: '제 17회 정기연주회'
     },
     {
-        year: 2010,
-        date: '03월 05일',
-        content: '제 14회 정기연주회'
+        year: 2011,
+        date: '03월 18일',
+        content: '제 16회 정기연주회'
     },
     {
         year: 2010,
@@ -150,14 +145,14 @@ export const HistoryContents = [
         content: '제 15회 정기연주회'
     },
     {
-        year: 2009,
+        year: 2010,
         date: '03월 05일',
-        content: '제 12회 정기연주회'
+        content: '제 14회 정기연주회'
     },
     {
         year: 2009,
-        date: '03월 07일',
-        content: '부동산 대학원 동문회 연주'
+        date: '09월 11일',
+        content: '제 13회 정기연주회'
     },
     {
         year: 2009,
@@ -166,13 +161,18 @@ export const HistoryContents = [
     },
     {
         year: 2009,
-        date: '09월 11일',
-        content: '제 13회 정기연주회'
+        date: '03월 07일',
+        content: '부동산 대학원 동문회 연주'
+    },
+    {
+        year: 2009,
+        date: '03월 05일',
+        content: '제 12회 정기연주회'
     },
     {
         year: 2008,
-        date: '03월 07일',
-        content: '제 10회 정기연주회'
+        date: '09월 04일',
+        content: '제 11회 정기연주회'
     },
     {
         year: 2008,
@@ -181,23 +181,13 @@ export const HistoryContents = [
     },
     {
         year: 2008,
-        date: '09월 04일',
-        content: '제 11회 정기연주회'
+        date: '03월 07일',
+        content: '제 10회 정기연주회'
     },
     {
         year: 2007,
-        date: '01월 14일',
-        content: '꽃동산교회 창작 뮤지컬 연주'
-    },
-    {
-        year: 2007,
-        date: '03월 09일',
-        content: '제 8회 정기연주회'
-    },
-    {
-        year: 2007,
-        date: '06월 29일',
-        content: '화서동 성당 연주회'
+        date: '09월 16일',
+        content: '제 9회 정기연주회'
     },
     {
         year: 2007,
@@ -206,23 +196,23 @@ export const HistoryContents = [
     },
     {
         year: 2007,
-        date: '09월 16일',
-        content: '제 9회 정기연주회'
+        date: '06월 29일',
+        content: '화서동 성당 연주회'
+    },
+    {
+        year: 2007,
+        date: '03월 09일',
+        content: '제 8회 정기연주회'
+    },
+    {
+        year: 2007,
+        date: '01월 14일',
+        content: '꽃동산교회 창작 뮤지컬 연주'
     },
     {
         year: 2006,
-        date: '03월 25일',
-        content: '제 6회 정기연주회'
-    },
-    {
-        year: 2006,
-        date: '05월 27일',
-        content: "영화 '신데렐라' 촬영"
-    },
-    {
-        year: 2006,
-        date: '07월 25일',
-        content: "웨이브TV '웨이브온투데이' 촬영"
+        date: '11월 17일',
+        content: '도서관 문화행사 연주'
     },
     {
         year: 2006,
@@ -231,18 +221,23 @@ export const HistoryContents = [
     },
     {
         year: 2006,
-        date: '11월 17일',
-        content: '도서관 문화행사 연주'
+        date: '07월 25일',
+        content: "웨이브TV '웨이브온투데이' 촬영"
+    },
+    {
+        year: 2006,
+        date: '05월 27일',
+        content: "영화 '신데렐라' 촬영"
+    },
+    {
+        year: 2006,
+        date: '03월 25일',
+        content: '제 6회 정기연주회'
     },
     {
         year: 2005,
-        date: '05월 11일',
-        content: '로맨틱 세레나데 (축제 이벤트)'
-    },
-    {
-        year: 2005,
-        date: '05월 12일',
-        content: '일감호 가요제 오프닝 연주'
+        date: '09월 08일',
+        content: '제 5회 정기연주회'
     },
     {
         year: 2005,
@@ -251,23 +246,18 @@ export const HistoryContents = [
     },
     {
         year: 2005,
-        date: '09월 08일',
-        content: '제 5회 정기연주회'
+        date: '05월 12일',
+        content: '일감호 가요제 오프닝 연주'
+    },
+    {
+        year: 2005,
+        date: '05월 11일',
+        content: '로맨틱 세레나데 (축제 이벤트)'
     },
     {
         year: 2004,
-        date: '04월',
-        content: '학생회 출범식 찬조 연주'
-    },
-    {
-        year: 2004,
-        date: '05월',
-        content: '가요제 찬조 연주'
-    },
-    {
-        year: 2004,
-        date: '05월 27일',
-        content: '제 3회 정기연주회'
+        date: '11월 24일',
+        content: '제 4회 정기연주회'
     },
     {
         year: 2004,
@@ -276,18 +266,28 @@ export const HistoryContents = [
     },
     {
         year: 2004,
-        date: '11월 24일',
-        content: '제 4회 정기연주회'
+        date: '05월 27일',
+        content: '제 3회 정기연주회'
     },
     {
-        year: 2003,
-        date: '02월',
-        content: '세레나데 창단'
-    },
-    {
-        year: 2003,
+        year: 2004,
         date: '05월',
-        content: '창단 연주회'
+        content: '가요제 찬조 연주'
+    },
+    {
+        year: 2004,
+        date: '04월',
+        content: '학생회 출범식 찬조 연주'
+    },
+    {
+        year: 2003,
+        date: '11월 26일',
+        content: '제 2회 정기연주회'
+    },
+   {
+        year: 2003,
+        date: '10월',
+        content: '영문인의 밤 축하 연주'
     },
     {
         year: 2003,
@@ -296,12 +296,13 @@ export const HistoryContents = [
     },
     {
         year: 2003,
-        date: '10월',
-        content: '영문인의 밤 축하 연주'
+        date: '05월',
+        content: '창단 연주회'
     },
     {
         year: 2003,
-        date: '11월 26일',
-        content: '제 2회 정기연주회'
+        date: '02월',
+        content: '세레나데 창단'
     },
+
 ];
