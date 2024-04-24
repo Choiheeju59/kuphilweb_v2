6970ca0ceb7fe3f71cbf2ae278e672b35ac9ded6
serin301 <63085375+serin301@users.noreply.github.com>
2024-04-25 01:48:05 +0900

    FEAT: 타이틀 영역 아카이브 클릭 시 페이지 이동

--- src/Pages/ArchiveRead.js
+++ src/Pages/ArchiveRead.js
@@ -15,6 +15,7 @@ const ArchiveRead = () => {
                     <TitleGradient
                         title="아카이브"
                         explain="KUPhil의 역대 연주회 정보를 모아놓은 공간입니다."
+                        link="/archive/1"
                         color="linear-gradient(91.48deg, #ECFDE5 0%, rgba(189, 222, 236, 0.31) 100%)"
                     />
                     <MainContent>
