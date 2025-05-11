53b9a1a494110c89720c71ec267418dee42d6038
rkddusen <k99endus@naver.com>
2025-05-11 17:00:12 +0900

    하드코딩 줄이기

--- src/Components/archive/ConcertInfoContent.js
+++ src/Components/archive/ConcertInfoContent.js
@@ -4,40 +4,31 @@ import { useNavigate, useParams } from "react-router-dom";
 
 import { getConcertData } from "../../utils/api";
 import styled from "styled-components";
-import { useScrollTopAlways } from '../../hooks/useScrollTop';
+import { useScrollTopAlways } from "../../hooks/useScrollTop";
 
 const ConcertInfoContent = () => {
   const { num } = useParams();
   const navigate = useNavigate();
   const [concertData, setConcertData] = useState(null);
   const [isValidNum, setIsValidNum] = useState(true);
+  const totalConcerts = 44;
+  const perPage = 6;
 
   useScrollTopAlways();
 
-  const handleGoBack = () => {    
+  const handleGoBack = () => {
     let page = getValue(num);
     navigate(`/archive/${page}`);
   };
 
   const getValue = (input) => {
-    const ranges = [
-        { min: 1, max: 5, value: 7 },
-        { min: 6, max: 11, value: 6 },
-        { min: 12, max: 17, value: 5 },
-        { min: 18, max: 23, value: 4 },
-        { min: 24, max: 29, value: 3 },
-        { min: 30, max: 35, value: 2 },
-        { min: 36, max: 42, value: 1 }
-    ];
+    if (input < 1 || input > totalConcerts) return;
 
-    for (const range of ranges) {
-        if (input >= range.min && input <= range.max) {
-            return range.value;
-        }
-    }
+    const reversedNum = totalConcerts - input;
+    const page = Math.floor(reversedNum / perPage) + 1;
 
-    return;
-}
+    return page;
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -50,142 +41,172 @@ const ConcertInfoContent = () => {
     };
 
     fetchData();
-    
   }, [num]);
 
   useEffect(() => {
     const parsedNum = parseInt(num, 10);
-    const isValid = !isNaN(parsedNum) && parsedNum >= 1 && parsedNum <= 42;
+    const isValid =
+      !isNaN(parsedNum) && parsedNum >= 1 && parsedNum <= totalConcerts;
     setIsValidNum(isValid);
-
   }, [num]);
 
-const songtitleObject = concertData && concertData.songtitle;
+  const songtitleObject = concertData && concertData.songtitle;
 
-const songtitleKeys = songtitleObject && Object.keys(songtitleObject);
+  const songtitleKeys = songtitleObject && Object.keys(songtitleObject);
 
-const songtitleList = songtitleKeys && songtitleKeys.map((key, index) => (
-  <div key={`ST${index}`}>
-    <Content style={{fontSize: "0.9em", color: "#444444"}}>{songtitleObject[key]}</Content>
-  </div>
-));
+  const songtitleList =
+    songtitleKeys &&
+    songtitleKeys.map((key, index) => (
+      <div key={`ST${index}`}>
+        <Content style={{ fontSize: "0.9em", color: "#444444" }}>
+          {songtitleObject[key]}
+        </Content>
+      </div>
+    ));
 
-const composerObject = concertData && concertData.composer;
+  const composerObject = concertData && concertData.composer;
 
-const composerKeys = composerObject && Object.keys(composerObject);
+  const composerKeys = composerObject && Object.keys(composerObject);
 
-const composerList = composerKeys && composerKeys.map((key, index) => (
-  <div key={`C${index}`}>
-    <Content style={{fontSize: "0.9em"}}>{composerObject[key]}</Content>
-  </div>
-));
+  const composerList =
+    composerKeys &&
+    composerKeys.map((key, index) => (
+      <div key={`C${index}`}>
+        <Content style={{ fontSize: "0.9em" }}>{composerObject[key]}</Content>
+      </div>
+    ));
 
-const pairedList = songtitleList && songtitleList.map((songtitle, index) => (
-  <div key={`P${index}`}>
-    {composerList && composerList[index]}
-    {songtitle}
-    <br/>
-    <br/>
-  </div>
-));
+  const pairedList =
+    songtitleList &&
+    songtitleList.map((songtitle, index) => (
+      <div key={`P${index}`}>
+        {composerList && composerList[index]}
+        {songtitle}
+        <br />
+        <br />
+      </div>
+    ));
 
   return (
     <>
-    {isValidNum ? (
-      <>
-        <div style={{display: "flex", marginBottom: "20px"}}>
-            {num < 42 && <Button onClick={() => navigate(`/concert/${parseInt(num, 10) + 1}`)}><BiChevronLeft/><div>다음 연주회</div></Button>}
+      {isValidNum ? (
+        <>
+          <div style={{ display: "flex", marginBottom: "20px" }}>
+            {num < 42 && (
+              <Button
+                onClick={() => navigate(`/concert/${parseInt(num, 10) + 1}`)}
+              >
+                <BiChevronLeft />
+                <div>다음 연주회</div>
+              </Button>
+            )}
             <Button onClick={handleGoBack}>
-              <BiMenu/><div>목록으로</div>
+              <BiMenu />
+              <div>목록으로</div>
             </Button>
-            {num > 1 && <Button onClick={() => navigate(`/concert/${parseInt(num, 10) - 1}`)}><BiChevronRight/><div>이전 연주회</div></Button>}
-        </div>
-        <Wrap>
-          <PosterImg style={{backgroundImage: `url(../../../images/poster/poster_${num}.jpg)`}}/>
-          <WrapInfo>
-            <Title>제{num}회 정기 연주회</Title>
-            <WrapContent>
-              <ContentTitle>장소 </ContentTitle>
-              <Content>{concertData && concertData.place}</Content>
-            </WrapContent>
-            <WrapContent>
-              <ContentTitle>일시 </ContentTitle>
-              <Content>{concertData && concertData.date.slice(0, 10)}</Content>
-            </WrapContent>
-            <WrapContent>
-              <ContentTitle>지휘 </ContentTitle>
-              <Content>{concertData && concertData.conductor}</Content>
-            </WrapContent>
-            <Divider/>
-            <ContentTitle style={{marginBottom: "5px"}}>프로그램</ContentTitle>
-            <Content>{pairedList}</Content>
-          </WrapInfo>
-        </Wrap>
-      </>
-    ) : (
-      <></>
+            {num > 1 && (
+              <Button
+                onClick={() => navigate(`/concert/${parseInt(num, 10) - 1}`)}
+              >
+                <BiChevronRight />
+                <div>이전 연주회</div>
+              </Button>
+            )}
+          </div>
+          <Wrap>
+            <PosterImg
+              style={{
+                backgroundImage: `url(../../../images/poster/poster_${num}.jpg)`,
+              }}
+            />
+            <WrapInfo>
+              <Title>제{num}회 정기 연주회</Title>
+              <WrapContent>
+                <ContentTitle>장소 </ContentTitle>
+                <Content>{concertData && concertData.place}</Content>
+              </WrapContent>
+              <WrapContent>
+                <ContentTitle>일시 </ContentTitle>
+                <Content>
+                  {concertData && concertData.date.slice(0, 10)}
+                </Content>
+              </WrapContent>
+              <WrapContent>
+                <ContentTitle>지휘 </ContentTitle>
+                <Content>{concertData && concertData.conductor}</Content>
+              </WrapContent>
+              <Divider />
+              <ContentTitle style={{ marginBottom: "5px" }}>
+                프로그램
+              </ContentTitle>
+              <Content>{pairedList}</Content>
+            </WrapInfo>
+          </Wrap>
+        </>
+      ) : (
+        <></>
       )}
     </>
   );
 };
 
 const Wrap = styled.div`
-    display: flex;
-    gap: 40px;
-    width: 100%;
-    justify-content: center;
+  display: flex;
+  gap: 40px;
+  width: 100%;
+  justify-content: center;
 
-    @media screen and (max-width: 767px) {
-      flex-direction: column;
-      align-items: center;
-      gap: 20px;
-    }
-`
+  @media screen and (max-width: 767px) {
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+  }
+`;
 
 const PosterImg = styled.div`
-    width: 300px;
-    height: 420px;
-    background-size: cover;
-    background-position: center;
+  width: 300px;
+  height: 420px;
+  background-size: cover;
+  background-position: center;
 
-    @media screen and (max-width: 767px) {
-      width: 200px;
-      height: 280px;
-    }
-`
+  @media screen and (max-width: 767px) {
+    width: 200px;
+    height: 280px;
+  }
+`;
 
 const WrapInfo = styled.div`
-    text-align: left;
-    width: 375px;
-    height: auto;
+  text-align: left;
+  width: 375px;
+  height: auto;
 
-    @media screen and (max-width: 767px) {
-      width: 250px;
-      height: auto;
-    }
-`
+  @media screen and (max-width: 767px) {
+    width: 250px;
+    height: auto;
+  }
+`;
 
 const Title = styled.div`
-    font-weight: bold;
-    font-size: 1.5rem;
-    margin-bottom: 20px;
+  font-weight: bold;
+  font-size: 1.5rem;
+  margin-bottom: 20px;
 
-    @media screen and (max-width: 767px) {
-      font-size: 1.1rem;
-      text-align: center;
-    }
-`
+  @media screen and (max-width: 767px) {
+    font-size: 1.1rem;
+    text-align: center;
+  }
+`;
 
 const WrapContent = styled.div`
-    display: flex;
-    gap: 5px;
-    margin-bottom: 10px;
+  display: flex;
+  gap: 5px;
+  margin-bottom: 10px;
 
-    @media screen and (max-width: 767px) {
-      flex-direction: column;
-      align-items: center;
-    }
-`
+  @media screen and (max-width: 767px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`;
 
 const ContentTitle = styled.div`
   font-weight: bold;
@@ -194,14 +215,14 @@ const ContentTitle = styled.div`
     text-align: center;
     font-size: 0.8rem;
   }
-`
+`;
 
 const Content = styled.div`
   @media screen and (max-width: 767px) {
     text-align: center;
     font-size: 0.8rem;
   }
-`
+`;
 
 const Button = styled.div`
   display: flex;
@@ -215,10 +236,10 @@ const Button = styled.div`
   @media screen and (max-width: 767px) {
     font-size: 0.7rem;
   }
-`
+`;
 
 const Divider = styled.div`
-  width: 100%; 
+  width: 100%;
   height: 1px;
   background-color: #aaaaaa;
   margin-bottom: 7px;
@@ -226,6 +247,6 @@ const Divider = styled.div`
   @media screen and (max-width: 767px) {
     display: none;
   }
-`
+`;
 
 export default ConcertInfoContent;
--- src/Components/archive/PosterContent.style.js
+++ src/Components/archive/PosterContent.style.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const PosterContainer = styled.div`
   display: grid;
@@ -14,76 +14,77 @@ export const PosterContainer = styled.div`
     grid-template-columns: repeat(1, auto);
     gap: 7vw 4vw;
   }
-`
+`;
 
 export const Wrap = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 1vw;
+  display: flex;
+  flex-direction: column;
+  gap: 1vw;
 
-    @media screen and (max-width: 767px) {
-      gap: 2vw;
-    }
+  @media screen and (max-width: 767px) {
+    gap: 2vw;
+  }
 
-    @media screen and (max-width: 412px) {
-      gap: 3vw;
-    }
-`
+  @media screen and (max-width: 412px) {
+    gap: 3vw;
+  }
+`;
 
 export const WrapPoster = styled.div`
-    width: ${(props) => props.divWidth}px;
-    height: ${(props) => props.divWidth * 1.4}px;
-    min-width: 160px;
-    min-height: 224px;
-    max-width: 200px;
-    max-height: 280px;
-    transition: background-size 0.3s;
-    box-shadow: 2px 2px 5px #ccc;
-    background-size: cover;
-    background-position: center;
-    cursor: pointer;
-    
-    @media screen and (min-width: 768px) and (hover: hover) and (pointer: fine)  {
-      &:hover {
-        background-size: 103%;
-      }
-    }
-    
-    @media screen and (max-width: 767px) {
-      width: ${(props) => props.divWidth * 1.4}px;
-      height: ${(props) => props.divWidth * 1.96}px;
-    }
+  width: ${(props) => props.divWidth}px;
+  height: ${(props) => props.divWidth * 1.4}px;
+  min-width: 160px;
+  min-height: 224px;
+  max-width: 200px;
+  max-height: 280px;
+  transition: background-size 0.3s;
+  box-shadow: 2px 2px 5px #ccc;
+  background-size: 100% 100%;
+  background-position: center;
+  cursor: pointer;
+  border-radius: 10px;
 
-    @media screen and (max-width: 412px) {
-      width: ${(props) => props.divWidth * 3}px;
-      height: ${(props) => props.divWidth * 4.2}px;
+  @media screen and (min-width: 768px) and (hover: hover) and (pointer: fine) {
+    &:hover {
+      background-size: 110% 110%;
     }
-`
+  }
+
+  @media screen and (max-width: 767px) {
+    width: ${(props) => props.divWidth * 1.4}px;
+    height: ${(props) => props.divWidth * 1.96}px;
+  }
+
+  @media screen and (max-width: 412px) {
+    width: ${(props) => props.divWidth * 3}px;
+    height: ${(props) => props.divWidth * 4.2}px;
+  }
+`;
 export const WrapConcertNumber = styled.div`
-    display: flex;
-    width: 100%;
-    height: 3vw;
-    align-items: center;
-    justify-content: center;
-`
+  display: flex;
+  width: 100%;
+  height: 3vw;
+  align-items: center;
+  justify-content: center;
+`;
 
 export const ConcertNumber = styled.p`
-    cursor: pointer;
-    font-size: clamp(1rem, 1vw, 1.5rem);
-`
+  cursor: pointer;
+  font-size: clamp(1rem, 1vw, 1.5rem);
+`;
 
 export const WrapPageNum = styled.div`
-    margin-top: 70px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`
+  margin-top: 70px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 export const PageBtn = styled.button`
-    cursor: pointer;
-    border: none;
-    background: none;
-    font-size: 1.2rem;
-    width: 40px;
-    height: 40px;
-`
+  cursor: pointer;
+  border: none;
+  background: none;
+  font-size: 1.2rem;
+  width: 40px;
+  height: 40px;
+`;
